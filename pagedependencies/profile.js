fd9ba4b56f0b771dd07de60209ec350a9e21b2c8
Adekunle Adigun <adigon2006@gmail.com>
2018-06-01 15:19:51 -0500

    profile js updated

--- pagedependencies/profile.js
+++ pagedependencies/profile.js
@@ -16,6 +16,7 @@ $('#editaccount').html("Update Account");
 fullname.removeAttr("readonly").focus();
 email.removeAttr("readonly");
 phone.removeAttr("readonly");
+$('.profileinput').css("border","1px solid #dedede");
 }
 else if(valueintext === "Update Account")
 {
@@ -23,18 +24,22 @@ $('#editaccount').html("Edit Account");
 fullname.attr("readonly",true);
 email.attr("readonly",true);
 phone.attr("readonly",true);
+$('.profileinput').css("border","none");
+$('.passwordsection').hide();
 
 oldpassword = $('#password').val();
 newpassword = $('#newpassword').val();
+confirmpassword = $('#confirmpassword').val()
 
 // storevalue of data
 fullnameval = $('#fullname').val();
 emailval  = $('#email').val();
 phoneval  = $('#phone').val();
 
-if(oldpassword !== "" && oldpassword === newpassword)
+if(oldpassword !== "" && oldpassword !== newpassword && newpassword === confirmpassword)
 {
-changedpassword = oldpassword;
+changedpassword = newpassword;
+// changed password
 }
 //create a data array
 
@@ -49,10 +54,12 @@ changedpassword = oldpassword;
 
 
 // change password show toggler
-$('#changepassword').on("click", function(){
+$('#changepassword').on("click", function(e){
+  e.preventDefault();
 visiblesection = $('.passwordsection').is(":hidden");
 password  = $('#password');
 newpassword  = $('#newpassword');
+confirmpassword = $('#confirmpassword');
 // toggle the password section
 $('.passwordsection').slideToggle(100);
 
@@ -63,14 +70,18 @@ if(visiblesection)
   // console.log(true);
   password.removeAttr("readonly").focus();
   newpassword.removeAttr("readonly");
+  confirmpassword.removeAttr("readonly");
   // change the value of text in Edit Account
   $('#editaccount').html("Update Account");
+  $('.passinput').css("border","1px solid #dedede");
 }
 if(!visiblesection)
 {
   // console.log(false);
   password.attr("readonly",true).val("");
   newpassword.attr("readonly",true).val("");
+  confirmpassword.attr("readonly",true).val("");
+$('.passinput').css("border","none");
 }
 });
 
@@ -78,9 +89,15 @@ if(!visiblesection)
 fullname  = $('#fullname');
 email  = $('#email');
 phone  = $('#phone');
+password  = $('#password');
+newpassword  = $('#newpassword');
+confirmpassword = $('#confirmpassword');
 UpdateTextfiled(fullname);
 UpdateTextfiled(email);
 UpdateTextfiled(phone);
+UpdateTextfiled(password);
+UpdateTextfiled(newpassword );
+UpdateTextfiled(confirmpassword);
 
 
 
